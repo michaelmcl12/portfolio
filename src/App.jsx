8d55dca1a736f25054aba2c0a05d52c6d7c22a45
Michael McLaughlin <michael.mcl812@gmail.com>
2023-04-13 21:28:46 -0400

    Added Components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Components/Header/Header.jsx';
+import Nav from './Components/Nav/Nav.jsx';
+
+
+const App = () => {
+    return (
+        <>
+         <Header />
+         <Nav />
+        </>
+    )
+}
+
+export default App;
