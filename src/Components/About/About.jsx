1ffeed2d22cb62ef5f5ba0dd7e1253be6e653bf9
Michael McLaughlin <michael.mcl812@gmail.com>
2023-05-24 19:02:59 -0400

    Finished about and experience section.

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import './About.css'
+import Headshot from '../../Assets/headshot.jpg'
+import {TbAwardFilled} from 'react-icons/tb'
 
 const about = () => {
     return (
-        <section id='about'>about</section>
+        <section id='about'>
+            <h5>Get to Know</h5>
+            <h2>About Me</h2>
+
+            <div className='container about_container'>
+                <div className="about_me">
+                    <div className="about_me_image">
+                        <img src={Headshot} alt='headshot' />
+                    </div>
+                </div>
+
+                <div className="about_content">
+                    <div className="about_cards">
+                        <article className='about_card'>
+                            <TbAwardFilled className='about_icon' />
+                            <h5>Experience</h5>
+                            <small>3 years </small>
+                        </article>
+
+                        <article className='about_card'>
+                            <TbAwardFilled className='about_icon' />
+                            <h5>clients</h5>
+                            <small> a lot </small>
+                        </article>
+
+                        <article className='about_card'>
+                            <TbAwardFilled className='about_icon' />
+                            <h5>projects </h5>
+                            <small>55</small>
+                        </article>
+                    </div>
+
+                    <p>
+                        asdfodsjf
+                    </p>
+
+                    <a href='#contact' className='btn btn-primary'>Contact Me!</a>
+
+                </div>
+            </div>
+            
+        </section>
     )
 }
 
--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,9 +1,95 @@
 import React from 'react'
 import './Experience.css'
+import  LinearProgress  from  '@mui/material/LinearProgress'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    learning: {
+      main: '#ffbe0b',
+    },
+    intermediate: {
+      main: '#fb5607',
+    },
+    proficient: {
+      main: '#ff006e',
+    },
+    expert: {
+      main: '#8338ec',
+    },
+  },
+});
+
 
 const Experience = () => {
   return (
-    <section id='experience'>exp</section>
+    <ThemeProvider theme={theme}>
+      <section id='experience'>
+        <h5>My Skills</h5>
+        <h2>Experience</h2>
+
+        <div className="container exp_container">
+          <div className="exp_languages">
+            <h3>Programming Languages</h3>
+        
+            <div className="exp_content">
+              <article className='exp_details'>
+                <h4>Python</h4>
+                <LinearProgress color='proficient' variant="determinate" value={75} />
+              </article>
+              
+              <article className='exp_details'>
+                {/* logo */}
+                <h4>C++</h4>
+                <LinearProgress color='proficient' variant="determinate" value={75} />
+              </article>
+
+              <article className='exp_details'>
+                {/* logo */}
+                <h4>HTML</h4>
+                <LinearProgress color='proficient' variant="determinate" value={75} />
+              </article>
+
+              <article className='exp_details'>
+                {/* logo */}
+                <h4>CSS</h4>
+                <LinearProgress color='proficient' variant="determinate" value={75} />
+              </article>
+
+              <article className='exp_details'>
+                {/* logo */}
+                <h4>Javascript</h4>
+                <LinearProgress color='intermediate' variant="determinate" value={50} />
+              </article>      
+            </div>
+          </div>
+
+          <div className="exp_frameworks">
+            <h3>Other Skills</h3>
+            <div className="exp_content">
+              <article className='exp_details'>
+                {/* logo */}
+                <h4>React</h4>
+                <LinearProgress color='learning' variant="determinate" value={25} />
+              </article>
+
+              <article className='exp_details'>
+                {/* logo */}
+                <h4>Git</h4>
+                <LinearProgress color='intermediate' variant="determinate" value={50} />
+              </article>
+
+              <article className='exp_details'>
+                {/* logo */}
+                <h4>Django</h4>
+                <LinearProgress color='learning' variant="determinate" value={25} />
+              </article>
+          </div>
+          </div>
+
+        </div>
+      </section>
+    </ThemeProvider> 
   )
 }
 
