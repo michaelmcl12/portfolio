c7739f3f94009655e05d209046f763fba3e49860
Michael McLaughlin <michael.mcl812@gmail.com>
2023-05-31 15:09:27 -0400

    Worked on portfolio section. Have card outline done.

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Portfolio.css'
+import IMG1 from '../../Assets/portfoliio.jpg'
+import {BsGithub} from 'react-icons/bs'
 
 const Portfolio = () => {
     return (
-        <section id='portfolio'>portfolio</section>
+        <section id='portfolio'>
+            <h5>Recent Projects</h5>
+            <h2>Portfolio</h2>
+
+            <div className="container portfolio_container">
+                <article className='portfolio_card'>
+                    <h3>Portfolio Project</h3>
+                    <div className='portfolio_card_image'>
+                        <img src={IMG1} alt='portfolio image'></img>
+                    </div>
+
+                    <p>smalll descriptions sdjfhsdkjfh ds afhdsi hfishfioasd </p>
+                    <div className='icon'><a href='https://github.com' target='_blank'><BsGithub /></a></div>
+                </article>
+            </div>
+
+        </section>
     )
 }
 
